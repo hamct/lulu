4a8862475f59c4cb93130496a4b369ebb896ee7d
hamct <hmnpark2@gmail.com>
2025-06-23 23:33:48 -0500

    setup

---
+++ src/test/kotlin/party/cattery/lulu/ApplicationTests.kt
@@ -0,0 +1,13 @@
+package party.cattery.lulu
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
